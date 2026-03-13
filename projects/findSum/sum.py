a18d259ce1512ea3b6cd7bddc0b56b727596ce9e
Shivaram <hrshivram@gmail.com>
2026-03-13 20:08:38 +0530

    Seacrh by content added

--- projects/findSum/sum.py
+++ projects/findSum/sum.py
@@ -1,3 +1,4 @@
+# sum of two numbers
 def Sum(a, b):
     return a + b
 print(Sum(1, 2))
--- pyfind.py
+++ pyfind.py
@@ -23,20 +23,31 @@ def searchFilePattern(dir, pattern):
 def searchFileExtension(dir, extension):
     return searchFilePattern(dir, f"*.{extension}")
 
+def searchFileContents(dir, content):
+    sameFileContent = []
+    for root, _, files in os.walk(dir):
+        for file in files:
+            if file.startswith("._"):
+                continue
+            try:
+                with open(os.path.join(root, file), 'r', errors='ignore') as f:
+                    if content in f.read():
+                        sameFileContent.append(os.path.join(root, file))
+            except:
+                continue
+    return sameFileContent
+
 
 def execute():
     if len(sys.argv) == 1:
         print("No arguments provided")
         sys.exit(1)
     currDir = os.getcwd()
-    if '.' not in sys.argv[1] and len(sys.argv) == 2:
-        fileName = sys.argv[1]
-        newDir = os.path.join(currDir, fileName)
-        getFilePath(newDir)
-
-    fileName = sys.argv[sys.argv.index('.')-1]
+    fileName = sys.argv[1]
     newDir = os.path.join(currDir, fileName)
-
+    if len(sys.argv) == 2:
+        getFilePath(newDir)
+        return
 
     if "--name" in sys.argv:
         pattern = sys.argv[sys.argv.index("--name") + 1]
@@ -48,7 +59,11 @@ def execute():
         sameFileExt = searchFileExtension(newDir, extension)
         for ele in sameFileExt:
             print(ele)
-
+    if "--contains" in sys.argv:
+        content = sys.argv[sys.argv.index("--contains") + 1]
+        sameFileContents = searchFileContents(newDir, content)
+        for path in sameFileContents:
+            print(path)
 
 if __name__ == "__main__": 
     execute()
