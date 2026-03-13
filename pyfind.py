03452305a2e36b5ff0c2d900de04207027b3b340
Shivaram <hrshivram@gmail.com>
2026-03-13 17:16:30 +0530

    search file by pattern

--- pyfind.py
+++ pyfind.py
@@ -1,16 +1,40 @@
 import os
 import sys
-
-currDir = os.getcwd()
-if len(sys.argv) == 1:
-    print("No arguments provided")
-    sys.exit(1)
-fileName = sys.argv[1]
-newDir = os.path.join(currDir, fileName)
+import fnmatch
 
 
-for root, dirs, files in os.walk(newDir):
-    for file in files:
-        if file.startswith("._"):
-            continue
-        print(os.path.join(root, file))
+def getFilePath(dir):
+    for root, _, files in os.walk(dir):
+        for file in files:
+            if file.startswith("._"):
+                continue
+            print(os.path.join(root, file))
+
+def searchFilePattern(dir, pattern):
+    sameFileTypes = []
+    for root, _, files in os.walk(dir):
+        for file in files:
+            if file.startswith("._"):
+                continue
+            if fnmatch.fnmatch(file, pattern):
+                sameFileTypes.append(os.path.join(root, file))
+    return sameFileTypes
+
+
+def execute():
+    if len(sys.argv) == 1:
+        print("No arguments provided")
+        sys.exit(1)
+    currDir = os.getcwd()
+    if '.' not in sys.argv[1] and len(sys.argv) == 2:
+        fileName = sys.argv[1]
+        newDir = os.path.join(currDir, fileName)
+        getFilePath(newDir)
+    if "--name" in sys.argv:
+        pattern = sys.argv[sys.argv.index("--name") + 1]
+        sameFileTypes = searchFileExtension(currDir, pattern)
+        for ele in sameFileTypes:
+            print(ele)
+        
+if __name__ == "__main__": 
+    execute()
